82ab78343648b8c5ed0938d692ff7721fc20c259
matheus-organek <matheusorganek@icloud.com>
2024-01-16 21:29:40 -0300

    Job Class has been created

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Job
+{
+    public string _company = "";
+    public string _jobTitle = "";
+    public double _startYear;
+    public double _endYear;
+
+    public Job()
+    {
+    }
+
+    public void DisplayJobDetails()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
