16b5af084b2ccffe806d1482f3d6cc3ddfe61815
matheus-organek <matheusorganek@icloud.com>
2024-01-22 20:21:08 -0300

    Error fixed

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-         Fraction f1 = new Fraction();
+        Fraction f1 = new Fraction();
         Console.WriteLine(f1.GetFractionString());
         Console.WriteLine(f1.GetDecimalValue());
 
