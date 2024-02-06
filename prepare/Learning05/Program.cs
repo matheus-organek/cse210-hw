536b9dd21b2450d7af8fa1d3c91f41df0d7d3c79
matheus-organek <matheusorganek@icloud.com>
2024-02-06 20:43:26 -0300

    Week 05 Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("White", 4);
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("Black", 4, 3);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("Grenn", 4);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.Color;
+
+            double computedArea = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {computedArea}.");
+        }
     }
 }
